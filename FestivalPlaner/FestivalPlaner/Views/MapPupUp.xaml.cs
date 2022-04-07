9e6727739d938c40faf3bc6247e80cce9afce051
vale-sch <vali.vogt@live.de>
2022-04-07 19:43:25 +0200

    adding a pin on map by click event implementation

--- FestivalPlaner/FestivalPlaner/Views/MapPupUp.xaml.cs
+++ FestivalPlaner/FestivalPlaner/Views/MapPupUp.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.CommunityToolkit.UI.Views;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace FestivalPlaner.Views
@@ -14,11 +15,27 @@ namespace FestivalPlaner.Views
         public MapPupUp()
         {
             InitializeComponent();
+            BindingContext = this;    
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
             Dismiss(null);
+            
+           
+        }
+
+        private void Map_MapClicked(object sender, MapClickedEventArgs e)
+        {
+            var pin = new Pin
+            {
+                Type = PinType.Place,
+                Position = new Position(Convert.ToDouble(e.Position.Latitude), Convert.ToDouble(e.Position.Longitude)),
+                Label = "custom pin",
+                Address = "custom detail info"
+            };
+         
+            Map.Pins.Add(pin);
         }
     }
 }
--- FestivalPlaner/FestivalPlaner/Views/NewItemPage.xaml.cs
+++ FestivalPlaner/FestivalPlaner/Views/NewItemPage.xaml.cs
@@ -3,6 +3,7 @@ using FestivalPlaner.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using Xamarin.CommunityToolkit.Extensions;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,12 +11,20 @@ namespace FestivalPlaner.Views
 {
     public partial class NewItemPage : ContentPage
     {
-        public FestivalModel Item { get; set; }
+        //public FestivalModel Item { get; set; }
 
         public NewItemPage()
         {
             InitializeComponent();
             BindingContext = new NewItemViewModel();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.ShowPopup(new MapPupUp()
+            {
+                IsLightDismissEnabled = false
+            });
         }
     }
 }
